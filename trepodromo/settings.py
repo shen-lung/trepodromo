72c64a75097c04d741305c81ddd919acca80cb2c
vadim <banzabit@gmail.com>
2018-05-31 21:55:50 -0300

    UPdate

--- trepodromo/settings.py
+++ trepodromo/settings.py
@@ -26,9 +26,6 @@ SECRET_KEY = '1$v-2(4s=g!$6crq4*#cv$w(o19elb&-qr^kixx#luyh9hx@^j'
 DEBUG = TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = [
-    '0.0.0.0',
-    '192.168.1.105',
-    '192.168.1.104',
     '127.0.0.1',
     'localhost',
     'www.trepodromo.com',
