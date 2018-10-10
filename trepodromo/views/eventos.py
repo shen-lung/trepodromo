5be059ff068f1a017cb9108d01f0fbf622a0da31
vadim <banzabit@gmail.com>
2018-10-10 00:45:06 -0300

    Add festoboulder image file

--- trepodromo/views/eventos.py
+++ trepodromo/views/eventos.py
@@ -8,12 +8,14 @@ class EventosView(BaseView):
     def get(self, request):
         desktop_images_urls = self.get_image_urls('eventos/desktop')
         mobile_images_urls = self.get_image_urls('eventos/mobile')
+        events_images_urls = self.get_image_urls('eventos/eventos')
 
         return render_to_response(
             'eventos.html',
             {
                 'desktop_images_urls': desktop_images_urls,
                 'mobile_images_urls': mobile_images_urls,
+                'events_images_urls': events_images_urls,
                 'title': 'Eventos',
             }
         )
